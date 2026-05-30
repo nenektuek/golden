069aea85db5fda32c634b75ccce7cf57b74c75b2
nhfudzfsrzggt <loaderv1.nexa@gmail.com>
2026-05-30 12:23:21 +0700

    Update main.lua

--- dist/main.lua
+++ dist/main.lua
@@ -6,7 +6,7 @@ local Players     = game:GetService("Players")
 local LocalPlayer = Players.LocalPlayer
 local RunService  = game:GetService("RunService")
 
-local BASE = "https://raw.githubusercontent.com/nhfudzfsrzggt/brigida/refs/heads/main/"
+local BASE = "https://raw.githubusercontent.com/nhfudzfsrzggt/golden/refs/heads/main/"
 local function load(path) return loadstring(game:HttpGet(BASE .. path))() end
 local function loadUrl(url) return loadstring(game:HttpGet(url))() end
 
